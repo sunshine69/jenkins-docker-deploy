137a9eadbcabaed24ce4bcd5998038e67bd398cf
Steve Kieu <steve.kieu@xvt.com.au>
2018-08-16 11:05:04 +1000

    Always copy the cert over

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ ! -f docker-18.06.0-ce.tgz ]; then
-    wget https://s3-ap-southeast-2.amazonaws.com/xvt-public-repo/pub/docker-18.06.0-ce.tgz
+    wget -q https://s3-ap-southeast-2.amazonaws.com/xvt-public-repo/pub/docker-18.06.0-ce.tgz
 fi
 
-if [ -f xvt.technology.key ]; then
-    aws s3 cp s3://xvt-aws-secure-backups/jenkins/xvt.technology.key .
-    aws s3 cp s3://xvt-aws-secure-backups/jenkins/xvt.technology.crt .
-fi
+aws s3 cp s3://xvt-aws-secure-backups/jenkins/xvt.technology.key .
+aws s3 cp s3://xvt-aws-secure-backups/jenkins/xvt.technology.crt .
 
 if [ -z "$BUILD_NUMBER" ]; then
     export BUILD_NUMBER="$(date '+%Y%m%d%H%M%S')"
