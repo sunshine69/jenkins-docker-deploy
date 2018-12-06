17ca4c9ad4885f385cb7aa5ebf291104fe90e746
Gitea <gitea@fake.local>
2018-12-06 12:03:39 +1000

    Check aws credential before run
    We no longer allow the jenkins profile IAM to do anything thus we need this
    auth information for running this script.

--- build.sh
+++ build.sh
@@ -4,6 +4,15 @@ if [ ! -f docker-18.06.0-ce.tgz ]; then
     wget -q https://s3-ap-southeast-2.amazonaws.com/xvt-public-repo/pub/docker-18.06.0-ce.tgz
 fi
 
+echo "Enter your xvt-aws AWS_ACCESS_KEY_ID: (used for s3 copying ssl certs)"
+read AWS_ACCESS_KEY_ID
+echo Enter your xvt-aws AWS_SECRET_ACCESS_KEY:
+read -s AWS_SECRET_ACCESS_KEY
+
+[ -z "$AWS_ACCESS_KEY_ID" ] && echo "You need to export xvt-aws key and secret before running this script. This is used to s3 copy the xvt.technology ssl key and cert" && exit 1
+
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+
 aws s3 cp s3://xvt-aws-secure-backups/jenkins/xvt.technology.key .
 aws s3 cp s3://xvt-aws-secure-backups/jenkins/xvt.technology.crt .
 
