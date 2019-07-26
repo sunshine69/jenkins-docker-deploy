27822182c2bcc712a85d02269f8dc46397212268
Gitea <gitea@fake.local>
2019-07-26 15:31:14 +1000

    Allow to run the script anywhere
    by detecting the script dir

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -xe
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $SCRIPT_DIR
+
 if [ ! -f docker-18.06.0-ce.tgz ]; then
     wget -q https://s3-ap-southeast-2.amazonaws.com/xvt-public-repo/pub/docker-18.06.0-ce.tgz
 fi
