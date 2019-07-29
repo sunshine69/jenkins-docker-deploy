c3cd17f580ee403a265cf469119111223ba0ed1b
Gitea <gitea@fake.local>
2019-07-29 15:12:26 +1000

    Update the certificate update logic

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
 if [ "$1" == 'update-cert' ]; then
-    TEST_UPADTE=$(wget https://xvt-public-repo.s3-ap-southeast-2.amazonaws.com/pub/certs/xvt.technology.state -O -)
+    TEST_UPADTE=$(wget https://xvt-public-repo.s3-ap-southeast-2.amazonaws.com/pub/certs/jenkins.xvt.technology.state -O -)
     echo $TEST_UPADTE
     if [ "$TEST_UPADTE" != 'NEED-UPDATE' ]; then
         exit 0
@@ -41,4 +41,9 @@ if [ "$ans" = 'y' ]; then
     docker stop xvt_jenkins || true
     docker rm xvt_jenkins || true
     docker run --detach --restart always --name xvt_jenkins -p 4343:4343 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /dev/net/tun:/dev/net/tun --add-host="docker-host.xvt.internal:10.100.9.14" -v jenkins_home:/var/jenkins_home jenkins/xvt-jenkins:latest
+
+    echo 'UP-TO-DATE' > /tmp/jenkins.xvt.technology.state
+    aws s3 cp /tmp/jenkins.xvt.technology.state s3://xvt-public-repo/pub/certs/jenkins.xvt.technology.state --profile xvt-public-repo
+    rm -f /tmp/jenkins.xvt.technology.state
+
 fi
