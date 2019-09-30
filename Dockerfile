62f57cd609c13a3c568cf4c6e6e387135760d4e0
Gitea <gitea@fake.local>
2019-09-30 15:26:19 +1000

    Update the build script
    - allow action update-cert to only rebuild the image with updating certs.

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,20 @@ FROM jenkins/jenkins:lts
 # Status: https working
 
 # Install sudo to allow to run sudo docker command inside.
+
+ARG update_all=yes
+
 USER root
-RUN apt-get update \
-      && apt-get install -y sudo python-pip \
-      && rm -rf /var/lib/apt/lists/*
+
+RUN if [ "$update_all" = "yes" ]; then \
+    apt-get update \
+    && apt-get install -y sudo python-pip \
+    && rm -rf /var/lib/apt/lists/*; fi
+
 RUN echo "jenkins ALL=NOPASSWD: ALL" >> /etc/sudoers
-RUN pip install ansible awscli botocore boto3
+
+RUN if [ "$update_all" = "yes" ]; then pip install ansible awscli botocore boto3; fi
+
 COPY docker-18.06.0-ce.tgz /tmp/docker-18.06.0-ce.tgz
 RUN mkdir -p /tmp/1 && tar xzf /tmp/docker-18.06.0-ce.tgz -C /tmp/1 && mv /tmp/1/docker/* /usr/bin/ && rm -rf /tmp/1 /tmp/docker-18.06.0-ce.tgz
  
@@ -22,7 +30,9 @@ COPY xvt.technology.crt /var/lib/jenkins/cert
 COPY xvt.technology.key /var/lib/jenkins/pk
 COPY executors.groovy /usr/share/jenkins/ref/init.groovy.d/executors.groovy
 COPY jenkins-plugins.list /usr/share/jenkins/ref/jenkins-plugins.list 
-RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/jenkins-plugins.list
+
+RUN if [ "$update_all" = "yes" ]; then \
+    /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/jenkins-plugins.list; fi
 
 # These crt and key needs to supplied at docker run command.
 ENV JENKINS_OPTS --httpPort=-1 --httpsPort=4343 --httpsCertificate=/var/lib/jenkins/cert --httpsPrivateKey=/var/lib/jenkins/pk -Dio.jenkins.plugins.artifact_manager_jclouds.s3.S3BlobStoreConfig.deleteArtifacts=true
--- build.sh
+++ build.sh
@@ -8,14 +8,28 @@ if [ ! -f docker-18.06.0-ce.tgz ]; then
     wget -q https://s3-ap-southeast-2.amazonaws.com/xvt-public-repo/pub/docker-18.06.0-ce.tgz
 fi
 
+sudo cp /etc/ssl/xvt.technology.* .
+
+if [ "$1" = "update-cert" ]; then
+    STATUS=$(aws s3 cp s3://xvt-public-repo/pub/certs/jenkins.xvt.technology.state --profile xvt-public-repo -)
+    [ "x$STATUS" != "xNEED-UPDATE" ] && echo "Certs status is $STATUS. Do nothing" && exit 0
+    echo 'UP-TO-DATE' > /tmp/jenkins.xvt.technology.state
+    aws s3 cp /tmp/jenkins.xvt.technology.state s3://xvt-public-repo/pub/certs/jenkins.xvt.technology.state --profile xvt-public-repo
+    rm -f /tmp/jenkins.xvt.technology.state
+    update_all=no
+else
+    echo "Update jenkins? y/n"
+    read ans
+    [ "$ans" != "y" ] && echo "Aborted by user" && exit 1
+    update_all=yes
+    docker pull jenkins/jenkins:lts
+fi
+
 if [ -z "$BUILD_NUMBER" ]; then
     export BUILD_NUMBER="$(date '+%Y%m%d%H%M%S')"
 fi
 
-sudo cp /etc/ssl/xvt.technology.* .
-
-docker pull jenkins/jenkins:lts
-docker build -t jenkins/xvt-jenkins:${BUILD_NUMBER} .
+docker build -t jenkins/xvt-jenkins:${BUILD_NUMBER} --build-arg update_all=$update_all .
 docker tag jenkins/xvt-jenkins:${BUILD_NUMBER} jenkins/xvt-jenkins:latest
 
 echo "Stop and start jenkins container? y/n"
@@ -28,9 +42,4 @@ if [ "$ans" = 'y' ]; then
         -v /dev/net/tun:/dev/net/tun \
         -v jenkins_home:/var/jenkins_home \
         --add-host="docker-host.xvt.internal:10.100.9.95" jenkins/xvt-jenkins:latest
-
-    echo 'UP-TO-DATE' > /tmp/jenkins.xvt.technology.state
-    aws s3 cp /tmp/jenkins.xvt.technology.state s3://xvt-public-repo/pub/certs/jenkins.xvt.technology.state --profile xvt-public-repo
-    rm -f /tmp/jenkins.xvt.technology.state
-
 fi
